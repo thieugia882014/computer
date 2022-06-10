7a0f2e6fca4c771f7692edea08f6d1a93a0fda52
diepminhtuan <tuandmth2007019@fpt.edu.vn>
2022-06-10 10:17:53 +0700

    Initial commit

---
+++ src/test/java/aptech/computer/ComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package aptech.computer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
